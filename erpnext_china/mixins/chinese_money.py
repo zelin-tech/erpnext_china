3ef828d5e0f4e7f369e9aa878acb879696a9f7bf
qinyanwan <qinyanwan@qq.com>
2026-05-07 12:09:23 +0800

    修改chinese_money.py

--- erpnext_china/mixins/chinese_money.py
+++ erpnext_china/mixins/chinese_money.py
@@ -1,4 +1,6 @@
 import frappe
+from decimal import Decimal
+
 from erpnext_china.utils import cncurrency
 
 
@@ -11,8 +13,15 @@ class ChineseMoneyMixin:
             amount = self.rounded_total or self.grand_total
             base_amount = self.base_rounded_total or self.base_grand_total
 
-            self.in_words = cncurrency(amount, prefix=True)
-            self.base_in_words = cncurrency(base_amount, prefix=True)
+            self.in_words = cncurrency(
+                Decimal(str(amount)),
+                prefix=True
+            )
+
+            self.base_in_words = cncurrency(
+                Decimal(str(base_amount)),
+                prefix=True
+            )
 
         else:
             super().set_total_in_words()
