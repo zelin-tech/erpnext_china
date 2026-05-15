7f3c908066244008c9d9c33f15ad0c59b80de493
Fisher Yu <12823863+szufisher@users.noreply.github.com>
2026-05-15 15:14:45 +0800

    update icon path for v16

--- erpnext_china/hooks.py
+++ erpnext_china/hooks.py
@@ -12,8 +12,13 @@ after_install = "erpnext_china.setup.install.after_install"
 setup_wizard_requires = "assets/erpnext_china/js/setup_wizard.js"
 
 app_include_icons = [
-    "erpnext_china/icons/account_report.svg",
-	"erpnext_china/icons/cn_account_report.svg",
+    "/assets/erpnext_china/icons/account_report.svg",
+	"/assets/erpnext_china/icons/cn_account_report.svg"
+]
+
+web_include_icons = [
+    "/assets/erpnext_china/icons/account_report.svg",
+	"/assets/erpnext_china/icons/cn_account_report.svg"
 ]
 
 doctype_js = {
